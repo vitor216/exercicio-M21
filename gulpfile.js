11b776bd4fc8963001e73028489f437e5b2721ec
vitor <eletrotecvitor@gmail.com>
2023-07-25 00:21:02 -0300

    a

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -19,4 +26,5 @@ exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.js', gulp.parallel(scripts))
 }
